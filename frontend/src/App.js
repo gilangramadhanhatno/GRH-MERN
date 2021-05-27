30c42803b40c24cb2667431af8402b8135d907d7
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-05-27 21:15:56 +0700

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Initial Step</h1>
+    </div>
+  );
+}
+
+export default App;
