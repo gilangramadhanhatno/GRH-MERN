25d58c81f62cdd4cb7caaa169de207ffe7bb40e4
gilangramadhahatno <gilangramadhanhatno@gmail.com>
2021-07-05 20:34:12 +0700

    Testing API Detail Page

--- backend/test/index.js
+++ backend/test/index.js
@@ -13,11 +13,34 @@ describe("API ENDPOINT TESTING", () => {
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
-        done();
         expect(res.body).to.have.property("category");
         expect(res.body.category).to.have.an("array");
         expect(res.body).to.have.property("brand");
         expect(res.body.brand).to.have.an("array");
+        done();
+      });
+  });
+
+  it("GET Detail Page", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/member/detail-page/5e96cbe292b97300fc902225")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        // expect(res).to.be.an("object");
+        // expect(res.body).to.have.property("category");
+        // expect(res.body.category).to.have.an("array");
+        expect(res.body).to.have.property("imageId");
+        expect(res.body.imageId).to.have.an("array");
+        expect(res.body).to.have.property("_id");
+        expect(res.body).to.have.property("title");
+        expect(res.body).to.have.property("price");
+        expect(res.body).to.have.property("categoryId");
+        expect(res.body).to.have.property("__v");
+        expect(res.body).to.have.property("bank");
+        expect(res.body.bank).to.have.an("array");
+        done();
       });
   });
 });
